8adbc788c58151b228aab627333ec8ca4d196698
hanumanjeeshivam <shivammacate@gmail.com>
2020-05-02 09:26:16 -0700

    created class MasterBranchClassTest in master branch

--- src/test/java/GitHubTest.java
+++ src/test/java/GitHubTest.java
@@ -1,5 +1,10 @@
 public class GitHubTest {
     public static void main(String[] args) {
         System.out.println("Testing");
+        print();
+
+    }
+    public static void print(){
+        System.out.println("This is my first method");
     }
 }
---
+++ src/test/java/MasterBranchClassTest.java
@@ -0,0 +1,6 @@
+public class MasterBranchClassTest {
+
+    public static void main(String[] args) {
+        System.out.println("bug is fixed");
+    }
+}
