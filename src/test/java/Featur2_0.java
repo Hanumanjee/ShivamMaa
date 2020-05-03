c0a2dd01068cdc8365302ae194ec4a02ac25ba85
hanumanjeeshivam <shivammacate@gmail.com>
2020-05-02 22:01:02 -0700

    added one more line in master branch

--- src/test/java/Featur2_0.java
+++ src/test/java/Featur2_0.java
@@ -2,5 +2,8 @@ public class Featur2_0 {
     public static void main(String[] args) {
 
         //this is added in feature2.0 branch
+
+
+        //this comment is added after getting marge from master branch
     }
 }
