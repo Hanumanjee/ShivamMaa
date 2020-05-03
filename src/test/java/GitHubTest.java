77ebc1ace2b601d06ece4c80e209c1b27ab4a46d
hanumanjeeshivam <shivammacate@gmail.com>
2020-05-02 20:54:38 -0700

    work on featur2.0

--- src/test/java/GitHubTest.java
+++ src/test/java/GitHubTest.java
@@ -4,10 +4,15 @@ public class GitHubTest {
         print();
 
         //commit
+        feature();
 
     }
     public static void print(){
         System.out.println("This is my first method");
         System.out.println("test");
+    }
+
+    public static void feature(){
+        System.out.println("testing feature branch");
     }
 }
